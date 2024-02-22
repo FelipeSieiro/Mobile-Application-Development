bc581e6f9aeedb752b267ade6e0e135631e80bb2
FelipeSieiro <felipedossantos0804@gmail.com>
2024-02-22 20:41:16 -0300

    feat:aprendendo funções se apresentando

---
+++ Aula 2/ApresenteSe.kt
@@ -0,0 +1,10 @@
+fun main(){
+
+    var idade = 19
+    var altura = 1.77
+    var inicial = 'F'
+
+    println("Eu tenho ${idade} anos")
+    println("Minha altura é de ${altura.toString()} metros")
+    println("Minha inicial é ${inicial}")
+}
