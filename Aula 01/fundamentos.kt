63c67330104dbd201ab4877554c78f508115d5af
FelipeSieiro <felipedossantos0804@gmail.com>
2024-02-22 20:38:19 -0300

    feat: iniciando com a linguagem Kotlin

---
+++ Aula 01/fundamentos.kt
@@ -0,0 +1,16 @@
+/*
+@author : Felipe Sieiro 
+Data da criação: 22/02/2024
+
+ */
+
+//definição de função
+ fun main(){
+
+    println("Ola, mundo!") 
+
+    println("Nova Mensagem")
+
+ }
+
+
---
+++ Aula 01/fundamentos.md
@@ -0,0 +1,6 @@
+# Fundamentos Kotlin
+
+Comentarios em Kotlin
+
+- /**/ - fazemos comentários em bloco
+- // - fazemos comentarios em uma linha
