46dd07aaeb005c9ccb48b58f0cff040e394301d1
FelipeSieiro <felipedossantos0804@gmail.com>
2024-02-26 21:48:14 -0300

    feat: introdução as variaveis no kotlin

---
+++ Aula 03/Variaveis.kt
@@ -0,0 +1,28 @@
+// Exemplo de Variaveis em Kotlin
+
+// fun main() {
+
+//     val nome = "Felipe" //variavel tipo string   - mutavel 
+//     var idade = 19      //Variavel tipo inteira  - imutavel 
+
+//     println("Ola, $nome! Voce tem $idade anos")                     //
+
+// }
+
+//DEClaração e Inicialização de Variaveis 
+// Int, Double, String, Boolean
+
+fun main(){
+
+    val numeroInteiro: Int= 12
+    val numeroDecimal: Double = 12.4
+    val texto: String  = "Este é um Texto"
+    val verdadeiro: Boolean = true
+    val falso: Boolean = false
+
+
+println("Numero Inteiro: $numeroInteiro")
+println("Numero Decimal: $numeroDecimal")
+println("Texto: $texto")
+println("Verdadeiro ou Falso: $verdadeiro ou $falso")
+}
