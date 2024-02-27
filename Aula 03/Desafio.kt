d88f99fd3f8d4d71f00708438fb42e71f87d6a28
FelipeSieiro <felipedossantos0804@gmail.com>
2024-02-26 22:49:04 -0300

    feat: Desafio media escolar

---
+++ Aula 03/Desafio.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val nomeAluno: String = "Felipe Sieiro"
+    var nota1: Double = 9.5
+    var nota2: Double = 7.0 
+    var nota3: Double = 10.0
+
+    val mediaFinal = (nota1 + nota2 + nota3/ 3)
+
+    println("Olá ${nomeAluno} a sua média final é $mediaFinal")
+}
